ed96052c80c5eb312d065391d33cf200140251cd
Jack Ogina <jackogina60@gmail.com>
2020-01-01 18:36:15 +0300

    init registers

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdint.h>
 
+// memory
 uint16_t memory[UINT16_MAX];
+
+// registers
+
+enum {
+    R_R0 = 0,
+    R_R1,
+    R_R2,
+    R_R3,
+    R_R4,
+    R_R5,
+    R_R6,
+    R_R7,
+    R_PC,
+    R_COND,
+    R_COUNT,
+};
+
+uint16_t reg[R_COUNT];
 
 int main(){
     return -1;
