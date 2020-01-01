43a7e7551ee981ecf21ab5eb562c4c3511189f49
Jack Ogina <jackogina60@gmail.com>
2020-01-01 18:56:02 +0300

    conditional flags

--- main.c
+++ main.c
@@ -21,6 +21,33 @@ enum {
 
 uint16_t reg[R_COUNT];
 
+enum
+{
+    OP_BR = 0, /* branch */
+    OP_ADD,    /* add  */
+    OP_LD,     /* load */
+    OP_ST,     /* store */
+    OP_JSR,    /* jump register */
+    OP_AND,    /* bitwise and */
+    OP_LDR,    /* load register */
+    OP_STR,    /* store register */
+    OP_RTI,    /* unused */
+    OP_NOT,    /* bitwise not */
+    OP_LDI,    /* load indirect */
+    OP_STI,    /* store indirect */
+    OP_JMP,    /* jump */
+    OP_RES,    /* reserved (unused) */
+    OP_LEA,    /* load effective address */
+    OP_TRAP    /* execute trap */
+};
+
+// conditional flags
+enum {
+    FLAG_POS = 1 << 0,
+    FLAG_ZRO = 1 << 1,
+    FLAG_NEG = 1 << 2, 
+};
+
 int main(){
     return -1;
 }
